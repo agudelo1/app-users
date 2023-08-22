f633ff7ac4e7e0aaf312ced6b8f289f7811b5e7b
unknown <cristian.agudelo1@udea.edu.co>
2023-08-22 15:20:24 -0500

    se agregaron alertas con sweetAlert2

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,9 +1,13 @@
 const UserCard = ({ user, deleteUser, handleClickUpdateUser }) => {
   return (
     <article className="bg-[#FFFFFF] border-[2px] drop-shadow-[0_35px_35px_rgba(0,0,0,0.20)] border-[#E5E5E5] w-[260px] rounded-md p-4 flex flex-col">
-      <h2 className="p-2 text-center mb-2 text-[20px] flex justify-center line-clamp-1 sm:line-clamp-1">
-        {user.first_name} {user.last_name}
+      <h2 className=" text-center mb-2 text-[20px] flex justify-center">
+        {user.first_name}
       </h2>
+      <h2 className="text-center mb-2 text-[20px] flex justify-center">
+        {user.last_name}
+      </h2>
+
       <hr className="border-[1px] border-[#E5E5E5]" />
       <ul className="p-2">
         <li className="grid gap-1 py-2  ">
