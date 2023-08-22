d00348aa2f6b2c1ccae71d5191e74a81a000007a
unknown <cristian.agudelo1@udea.edu.co>
2023-08-22 18:06:13 -0500

    se arreglo el responsive

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -29,7 +29,7 @@ const UserList = ({ users, deleteUser, handleClickUpdateUser }) => {
   }, [users]);
 
   return (
-    <section className="flex flex-col mx-auto  w-full max-w-[1024px]  ">
+    <section className="flex flex-col mx-auto  w-full max-w-[1200px]  ">
       <section className="grid grid-rows-[auto,auto] gap-8 items-center justify-center p-4 min-[640px]:grid-cols-[auto,auto] ">
         {usersInPage.map((user) => (
           <UserCard
